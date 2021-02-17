c3f913115daa74b7b7c3216a49b05005346806c6
doylej35 <doylej35@tcd.ie>
2021-02-17 21:59:32 +0000

    Added Google JSON file

--- app/src/main/java/com/example/workmate/ClientRegActivity.java
+++ app/src/main/java/com/example/workmate/ClientRegActivity.java
@@ -1,17 +1,15 @@
 package com.example.workmate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
