2d8b8bd5f8bb68a1be9428791066003961637b04
osullib8 <78975014+osullib8@users.noreply.github.com>
2021-02-23 21:22:35 +0000

    Update LoginFragment.java

--- app/src/main/java/com/example/workmate/LoginFragment.java
+++ app/src/main/java/com/example/workmate/LoginFragment.java
@@ -10,30 +10,8 @@ import androidx.fragment.app.Fragment;
 
 public class LoginFragment extends Fragment {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_profile, null);
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if(user != null){
-            String name = user.getDisplayName();
-
-            String uid = user.getUid();
-
-            TextView textView;
-            textView = (TextView) root.findViewById(R.id.textView3);
-            textView.setText(name);
-        }else{
-           Toast.makeText(getActivity(), "not logged in", Toast.LENGTH_SHORT).show();
-        }
-        /*firebaseDatabase = firebaseDatabase.getInstance();
-
-        databaseReference = firebaseDatabase.getReference("Data");
-
-        textView3 = textView3.findViewById(R.id.textView3);
-
-        getdata();*/
-
-        return root;
-        //return inflater.inflate(R.layout.fragment_profile,  container, false);
+    @Nullable
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_message,  container, false);
     }
 }
