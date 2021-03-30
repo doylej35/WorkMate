1b6189f16470baebe5b2fcc792ba0fb4e4245152
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-03-30 21:42:09 +0100

    Search Buttons Working

--- app/src/main/java/com/example/workmate/SearchFragment.java
+++ app/src/main/java/com/example/workmate/SearchFragment.java
@@ -100,7 +100,6 @@ public class SearchFragment extends Fragment {
 
         Toast.makeText(getActivity(),text, Toast.LENGTH_SHORT).show();
         
-        Log.d("CREATION","ARG TEXT = " + ARG_TEXT);
         Log.d("CREATION","TEXT = " + text);
 
         //supplierModelArrayList = dbHelper.readSuppliers();
