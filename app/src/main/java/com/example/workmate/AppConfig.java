6f79f18859d0e79045b28154216c1c89076bfdd9
igoea20 <64656258+igoea20@users.noreply.github.com>
2021-04-04 16:46:20 +0100

    added internet permissions

---
+++ app/src/main/java/com/example/workmate/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.workmate;
+
+public class AppConfig {
+    // Server client login url
+    public static String URL_CLIENT_LOGIN = "http://workmatedb.ddns.net/android_login_api/client_login.php";
+
+    //server supplier login url (not uploaded yet?)
+    public static String URL_SUPPLIER_LOGIN = "http://workmatedb.ddns.net/android_login_api/supplier_login.php";
+
+    // Server client register url
+    public static String URL_CLIENT_REGISTER = "http://workmatedb.ddns.net/android_login_api/client_register.php";
+
+    //server supplier register url
+    public static String URL_SUPPLIER_REGISTER = "http://workmatedb.ddns.net/android_login_api/supplier_register.php";
+
+}
