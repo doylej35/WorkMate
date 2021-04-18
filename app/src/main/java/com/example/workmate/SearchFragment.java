7dc79bd0b6f5d545a15c449fe0f5600c1379697c
Joe Marron <68810618+marronjo@users.noreply.github.com>
2021-04-18 20:08:53 +0100

    first test search function

--- app/src/main/java/com/example/workmate/SearchFragment.java
+++ app/src/main/java/com/example/workmate/SearchFragment.java
@@ -47,10 +47,10 @@ public class SearchFragment extends Fragment {
     private MainActivity mainHelper;
     private String input;
 
-    Context context = getActivity();
-    //final EditText Fname = findViewById(R.id.spinner);
+    private String searchInput;
+    private String spinnerInput;
 
-   public static SearchFragment newInstance(String text){
+    public static SearchFragment newInstance(String text){
          SearchFragment fragment = new SearchFragment();
          Bundle args = new Bundle();
          args.putString("ARG_TEXT",text);
@@ -62,50 +62,10 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_search,  container, false);
 
-
-        /*TextView textView = v.findViewById(R.id.search_prompt);
-
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_search, null);
-        EditText searchInput = root.findViewById(R.id.searchInput);
-      //  Log.d("CREATION", String.searchInput);
-
-        //creating a list of suppliers using the search function
-        DatabaseHelper databaseHelper= new DatabaseHelper(getActivity());
-        List<SupplierModel> suppliers = databaseHelper.searchService(searchInput.toString());
-        if(suppliers.size()>0) {
-            //showing search results in a toast
-            Toast.makeText(getActivity(), suppliers.toString(), Toast.LENGTH_SHORT).show();
-
-            //showing search results in logcat
-            Log.d("CREATION", String.valueOf(suppliers.get(0)));
-        } else {
-            //if there is nothing of this type in the database make a toast
-            Toast.makeText(getActivity(), "No suppliers of this kind", Toast.LENGTH_SHORT).show();
-            Log.d("CREATION", "NO SUPPLIERS OF THIS KIND");
-        }
-
-        if (getArguments() != null){
-            text = getArguments().getString("ARG_TEXT");
-        }
-
-        textView.setText(text);*/
-
-
         SearchView search = v.findViewById(R.id.search);
         Spinner spinner = v.findViewById(R.id.spinner);
-        
+
         Button button = v.findViewById(R.id.button2);
-        button.setOnClickListener(a->{
-                    if (search.toString().length() == 0  ){
-                        //no first name
-                       // search.setError("Please enter Name");
-
-                        Log.d("CREATION","Empty Search");
-                    }
-                }
-                );
-        
-
 
         DatabaseHelper dbHelper = new DatabaseHelper(getActivity());
 
@@ -139,7 +99,28 @@ public class SearchFragment extends Fragment {
 
         Log.d("CREATION","TEXT = " + text);
 
-        //supplierModelArrayList = dbHelper.readSuppliers();
+        button.setOnClickListener(task -> {
+            if (search.toString().equals("")){
+                supplierModelArrayList = dbHelper.readSuppliers();
+                //search.setError("Please enter Name");
+                Log.d("CREATION","Empty Search");
+            }
+            else{
+                searchInput = search.getQuery().toString();
+                spinnerInput = spinner.getSelectedItem().toString();
+
+                Log.d("CREATION",searchInput + " " + spinnerInput);
+                supplierModelArrayList = dbHelper.searchServiceFilter(searchInput, spinnerInput);
+                Log.d("CREATION",supplierModelArrayList.toString());
+                rvAdapter = new RVAdapter(supplierModelArrayList, getActivity());
+                suppliersRV = v.findViewById(R.id.idRVSuppliers);
+
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+                suppliersRV.setLayoutManager(linearLayoutManager);
+
+                suppliersRV.setAdapter(rvAdapter);
+            }
+        });
 
         rvAdapter = new RVAdapter(supplierModelArrayList, getActivity());
         suppliersRV = v.findViewById(R.id.idRVSuppliers);
