484b1acb58b107c358d06c958cefb51d894c7164
doylej35 <doylej35@tcd.ie>
2021-04-19 01:24:16 +0100

    splash_bar

---
+++ app/src/main/java/com/example/workmate/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.workmate.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workmate.MainActivity
+import com.example.workmate.R
+
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
